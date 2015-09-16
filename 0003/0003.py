ee09c6de20a0c0db35e067e58642fabe066aa53e
Jill Ye <jiyou.linux@gmail.com>
2015-09-16 09:46:31 +0800

    update 0003

--- 0003/0003.py
+++ 0003/0003.py
@@ -1,3 +1,47 @@
 # -*- coding: utf-8 -*-
 # 第 0003 题：将 0001 题生成的 200 个激活码（或者优惠券）保存到 Redis 非关系型数据库中。
 # fail to install redis, skip it
+import redis
+import uuid
+
+redis_server = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
+def create_code(number=200):
+    result = []
+    while True is True:
+        temp = str(uuid.uuid1()).replace('-', '')
+        if not temp in result:
+            result.append(temp)
+        if len(result) is number:
+            break
+    return result
+
+
+def cleanUp(prefix='showmethecode'):
+    keys = redis_server.keys('%s_*' % prefix)
+    for key in keys:
+        redis_server.delete(key)
+
+
+def insertCode(code, prefix='showmethecode'):
+    redis_server.set('%s_%s' % (prefix, code), code)
+
+
+def selectCodes(prefix='showmethecode'):
+    keys = redis_server.keys('%s_*' % prefix)
+    result = []
+    for key in keys:
+        result.append(redis_server.get(key))
+    return result
+
+
+def Process():
+    cleanUp()
+    code = create_code()
+    for c in code:
+        insertCode(c)
+    result = selectCodes()
+    return result
+
+Process()
---
+++ 0003/readme.md
@@ -0,0 +1,8 @@
+**第 0003 题：**将 0001 题生成的 200 个激活码（或者优惠券）保存到 **Redis** 非关系型数据库中。
+
+--------------
+redis for windows: 
+
+https://github.com/rgl/redis/downloads
+
+pip install redis (python -m pip install redis)
